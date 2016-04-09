4dfe2f6675c6ae4200bd4622852d823bed499c0f
Takeo Asai <spitson+github@gmail.com>
2016-04-09 17:33:51 +0900

    test

--- .githooks/commit-msg.rb
+++ .githooks/commit-msg.rb
@@ -1,10 +1,18 @@
 #!/usr/bin/env ruby
 
-commit_msgs = File.readlines(ARGV[0])
+message_file = ARGV[0]
+commit_msgs = []
+
+File.open(message_file, 'r').each_with_index do |line, line_number|
+    commit_msgs.push line
+    # e = check_format_rules line_number, line.strip
+    # errors.push e if e
+end
+
 
 open(ARGV[0], 'w') do |file|
     file.print "[#{$1}] "
     file.puts commit_msgs
 end
 
-exit(-1)
+exit(1)
